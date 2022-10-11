eb1ab35e2cf6e5f788d68648951fde740c1ba0dd
anniesyj <anniesong.yj@gmail.com>
2022-10-11 18:11:38 +0900

    edited post, and etc

--- models/post.js
+++ models/post.js
@@ -44,9 +44,6 @@ class Post extends Sequelize.Model {
                type: Sequelize.STRING(10),
                allowNull: false,
             },
-            category: {
-               type: Sequelize.STRING()
-            }
          },
          {  // 두번째 객체 인수는 테이블 자체에 대한 설정
             sequelize, /* static init 메서드의 매개변수와 연결되는 옵션으로, db.sequelize 객체를 넣어야 한다. */
